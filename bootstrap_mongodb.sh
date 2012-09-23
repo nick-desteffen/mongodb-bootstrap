00d96ac0658bf7892e2ae8c850fb5426742a97b3
Nick DeSteffen <nick.desteffen@gmail.com>
2012-09-23 08:02:19 -0500

    Updating after key has been added

--- bootstrap_mongodb.sh
+++ bootstrap_mongodb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sh -c "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >> /etc/apt/sources.list"
-apt-get -y update
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+apt-get -y update
 apt-get install mongodb-10gen
 mongo --version
